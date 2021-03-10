192d984bc2ecde1654831116c51ee4df8b1993e4
Maurice Payne <mauricetpayne@gmail.com>
2021-03-09 20:19:24 -0800

    Initial commit

---
+++ src/main/java/com/motoots/automator/AutomatorApplication.java
@@ -0,0 +1,13 @@
+package com.motoots.automator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motoots/automator/AutomatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoots.automator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
